72a16968a0cc1f423fbcbd1ae7ee164d0a91ad38
Udinkosd <126738691+Udinkosd@users.noreply.github.com>
2023-10-30 01:44:51 +0800

    Add files via upload

---
+++ esurat/src/main/java/controller/Main.java
@@ -0,0 +1,21 @@
+package controller;
+
+import gui.ChooseLogin;
+import java.awt.Dimension;
+
+public class Main {
+    public static void main(String[] args) {
+        ChooseLogin chooseLogin = new ChooseLogin();
+
+        chooseLogin.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        chooseLogin.setPreferredSize(new Dimension(800, 600));
+
+        chooseLogin.setVisible(true);
+    }
+
+    static class Model {
+
+        public Model() {
+        }
+    }
+}
